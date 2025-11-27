4ca6c7d390f3f2530ef2dcb0938f2ce3b355c7af
JGuzman87 <jguzman913@gmail.com>
2025-11-27 11:13:08 -0500

    separated data fetching to page instead of food component

--- src/app/foods/page.js
+++ src/app/foods/page.js
@@ -1,11 +1,74 @@
-
-import React from 'react';
+"use client"
+import { useEffect, useState} from 'react';
 import Foods from '@/components/Foods';
 
 const FoodPage = () => {
+
+  const [foodItem, setFoodItem] = useState("");
+    const [grams, setGrams] = useState("");
+    const [nutrition, setNutrition] = useState([]);
+  
+    useEffect(() => {
+      const fetchFoods = async () => {
+        const res = await fetch("/api/foods/db");
+        const data = await res.json();
+  
+        setNutrition(data);
+      };
+      fetchFoods();
+    }, []);
+
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+
+      if (name === 'grams') {
+        setGrams(value)
+      } else if(name === 'food') {
+        setFoodItem(value)
+      }
+    }
+  
+    const handleFetch = (e) => {
+      e.preventDefault();
+  
+      const fetchData = async () => {
+        const query = `${grams}g ${foodItem}`;
+        try {
+          const response = await fetch(
+            `/api/foods?query=${encodeURIComponent(query)}`
+          );
+          if (!response.ok) {
+            throw new Error(`Error: ${response.status} ${response.statusText}`);
+          }
+          const data = await response.json();
+  
+          let item = data.items[0];
+          setNutrition((prev) => [...prev, item]);
+          await fetch("/api/foods/db", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              name: item.name,
+              grams: item.serving_size_g,
+              calories: item.calories,
+              protein: item.protein_g,
+            }),
+          });
+          const updated = await fetch("/api/foods/db");
+          const updatedList = await updated.json();
+          setNutrition(updatedList);
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
+  
+      fetchData();
+      setFoodItem("");
+      setGrams("");
+    };
   return (
     <div className='grid grid-cols-3 mt-4 '>
-      <Foods />
+      <Foods foodFetch={handleFetch} nutrition={nutrition} foodItem={foodItem} grams={grams} formHandle={handleChange}/>
     </div>
   )
 }
--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -1,61 +1,8 @@
 "use client";
-import { useState, useRef, useEffect } from "react";
+import { useRef  } from "react";
 
-const Foods = () => {
-  const [foodItem, setFoodItem] = useState("");
-  const [grams, setGrams] = useState("");
-  const [nutrition, setNutrition] = useState([]);
-
-  useEffect(() => {
-    const fetchFoods = async () => {
-      const res = await fetch('/api/foods/db');
-      const data = await res.json();
-     
-        setNutrition(data);
-    }
-    fetchFoods();
-  }, [])
-
-
-  const handleFetch = (e) => {
-    e.preventDefault();
-
-    const fetchData = async () => {
-      const query = `${grams}g ${foodItem}`;
-      try {
-        const response = await fetch(
-          `/api/foods?query=${encodeURIComponent(query)}`
-        );
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status} ${response.statusText}`);
-        }
-        const data = await response.json();
-
-
-        let item = data.items[0];
-        setNutrition((prev) =>[...prev, item]);
-        await fetch("/api/foods/db", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            name: item.name,
-            grams: item.serving_size_g,
-            calories: item.calories,
-            protein: item.protein_g,
-          }),
-        });
-           const updated = await fetch("/api/foods/db");
-           const updatedList = await updated.json();
-           setNutrition(updatedList);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-
-    fetchData();
-    setFoodItem("");
-    setGrams("");
-  };
+const Foods = ({foodFetch, nutrition, foodItem, grams, formHandle}) => {
+  
 
   const modalRef = useRef(null);
 
@@ -78,14 +25,14 @@ const Foods = () => {
             X
           </button>
           <p className="text-center font-bold">Add Food Item</p>
-          <form className="flex flex-col gap-2 p-2" onSubmit={handleFetch}>
+          <form className="flex flex-col gap-2 p-2" onSubmit={foodFetch}>
             <label htmlFor="food">Food Item: </label>
             <input
               type="text"
               name="food"
               value={foodItem}
               placeholder="item.."
-              onChange={(e) => setFoodItem(e.target.value)}
+              onChange={formHandle}
               required
             />
             <label htmlFor="grams">Grams: </label>
@@ -94,7 +41,7 @@ const Foods = () => {
               name="grams"
               value={grams}
               placeholder="grams.."
-              onChange={(e) => setGrams(e.target.value)}
+              onChange={formHandle}
               required
             />
             <button type="submit" className="btn btn-success">
@@ -104,11 +51,19 @@ const Foods = () => {
         </div>
       </dialog>
 
-     
       {nutrition.length > 0 &&
-        nutrition.map((foodItem) => (
-          <div className="card bg-white shadow-2xl p-2 rounded-2xl" key={foodItem._id}>
+        nutrition.map((foodItem, index) => (
+          <div
+            className="card bg-white shadow-2xl p-2 rounded-2xl"
+            key={foodItem._id || index}
+          >
             <div className="card-body">
+              <button
+                type="button"
+                className="btn btn-ghost w-fit hover:bg-red-300 hover:text-white rounded-lg"
+              >
+                x
+              </button>
               <p className=" card-title capitalize text-center">
                 {" "}
                 {foodItem.name}
