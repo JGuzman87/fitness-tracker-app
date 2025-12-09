ee3d2f9bf0e7b5a91772023f079d057fe2ebc8ce
JGuzman87 <jguzman913@gmail.com>
2025-12-09 17:54:40 -0500

    added const {id} = params;

--- src/app/api/workouts/[id]/route.js
+++ src/app/api/workouts/[id]/route.js
@@ -4,8 +4,7 @@ import Workouts from "@/models/Workouts";
 export async function DELETE(_, { params }) {
   await connectDB();
 
-  const resolved = await params;
-  const id = resolved.id;
+  const { id } = params;
 
   try {
     await Workouts.findByIdAndDelete(id);
