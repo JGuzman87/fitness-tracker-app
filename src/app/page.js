9a96ff27c3a89697add2000c8a05516553ebd905
JGuzman87 <jguzman913@gmail.com>
2025-12-10 09:44:42 -0500

    removed height from homepage box text

--- src/app/page.js
+++ src/app/page.js
@@ -19,8 +19,8 @@ const Home = () => {
     <div className="flex flex-col justify-center items-center">
       <MotionWrapper>
         {!session ? (
-          <div className="w-fit h-50 flex flex-col justify-around items-center p-2 mt-20 bg-black/10 font-stretch-condensed rounded-xl  shadow-2xl text-3xl  font-light  ">
-            <p>
+          <div className="w-full flex flex-col justify-around items-center p-2 mt-20 bg-black/10 font-stretch-condensed rounded-xl  shadow-2xl text-3xl  font-light  ">
+            <p className='text-center'>
               Welcome to the My Fitness Tracker!
               <br /> Where you can track your workouts reps and log your meals
               to get calories and protein data.
